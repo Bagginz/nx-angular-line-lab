9337ccac532d56f28a8cc8d50e0b42c76600ef71
Pariwat <pariwat@theiconweb.com>
2020-02-03 23:04:53 +0700

    initial commit

---
+++ apps/angular-line-logiin/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'angular-line-logiin',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/angular-line-logiin',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
